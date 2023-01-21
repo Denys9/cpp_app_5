d885b63012f3ca3a1fa20774ad40972b2c743b97
lozin <lozindenis6@gmail.com>
2023-01-21 10:08:40 +0200

    Create work №1

--- main.cpp
+++ main.cpp
@@ -1,1 +1,19 @@
- 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int row, column;
+    cout << "row -  ";
+    cin >> row;
+    cout << "col -  ";
+    cin >> column;
+    char sym;
+    cout << "sym - ";
+    cin >> sym;
+    for (int i = 1; i <= row; i++) {
+        for (int j = 1; j <= column; j++)
+            cout << sym;
+        cout << endl;
+    }
+    return 0;
+}
