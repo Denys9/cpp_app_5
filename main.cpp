70f61877ee3e567d415430b02274929eaf5db4aa
lozin <lozindenis6@gmail.com>
2023-01-21 10:56:48 +0200

    Create work №4

--- main.cpp
+++ main.cpp
@@ -1,1 +1,16 @@
- 
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <random>
+int main()
+{
+    int arr[] = { rand()};
+
+    std::reverse(std::begin(arr), std::end(arr));
+
+    for (int& i : arr) {
+        std::cout << i << ' ';
+    }
+
+    return 0;
+}
